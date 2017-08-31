b5719d170266d1408ce6b0dc535ee55f713c86aa
Jonathan Melian <jonathanmelian@gmail.com>
2017-08-31 17:45:56 -0400

    For andreth's review

--- src/main/java/com/workshop/interceptor/HelloWorldInterceptor.java
+++ src/main/java/com/workshop/interceptor/HelloWorldInterceptor.java
@@ -1,37 +1,34 @@
-package com.workshop.interceptor;
-
-import com.workshop.interceptor.validators.Validator;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.ValidationException;
-import java.util.Optional;
-
-
-@Service
-public class HelloWorldInterceptor extends HandlerInterceptorAdapter {
-
-    @Autowired
-    private Validator[] validators;
-
-    @Override
-    public boolean preHandle(HttpServletRequest request,
-                             HttpServletResponse response, Object handler) throws Exception {
-
-        for(Validator validator : validators){
-            Optional<String> header = Optional.ofNullable(request.getHeader(validator.getHeaderKey()));
-            if (header.isPresent()) {
-                if (!header.get().equals(validator.getHeaderValue())) {
-                    throw new ValidationException(validator.getErrorMessage());
-                }
-            }else {
-                throw new ValidationException(validator.getHeaderKey() + " is missing or empty");
-            }
-        }
-        return true;
-    }
-
-}
+//package com.workshop.interceptor;
+//
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//
+//
+//@Service
+//public class HelloWorldInterceptor extends HandlerInterceptorAdapter {
+////
+////    @Autowired
+////    private Validator[] validators;
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request,
+//                             HttpServletResponse response, Object handler) throws Exception {
+//
+//        System.out.println(" Request auth " + request.getHeader("Authorization") );
+////        for(Validator validator : validators){
+////            Optional<String> header = Optional.ofNullable(request.getHeader(validator.getHeaderKey()));
+////            if (header.isPresent()) {
+////                if (!header.get().equals(validator.getHeaderValue())) {
+////                    throw new ValidationException(validator.getErrorMessage());
+////                }
+////            }else {
+////                throw new ValidationException(validator.getHeaderKey() + " is missing or empty");
+////            }
+////        }
+//        return true;
+//    }
+//
+//}
--- src/main/java/com/workshop/rest/HelloWorldController.java
+++ src/main/java/com/workshop/rest/HelloWorldController.java
@@ -1,15 +1,8 @@
 package com.workshop.rest;
 
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.validation.ValidationException;
-import java.io.IOException;
 
 @RestController
 public class HelloWorldController {
@@ -22,10 +15,10 @@ public class HelloWorldController {
     }
 
 
-    @ExceptionHandler(ValidationException.class)
-    protected void handleHelloWorldValidationException(ValidationException ex,
-                                                         HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
-    }
+//    @ExceptionHandler(ValidationException.class)
+//    protected void handleHelloWorldValidationException(ValidationException ex,
+//                                                         HttpServletRequest request, HttpServletResponse response) throws IOException {
+//        response.sendError(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+//    }
 
 }
